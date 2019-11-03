6146416a19eec19b26f825259d70dec20ebae916
Divyum <divz7777@gmail.com>
2019-11-04 03:29:22 +0530

    Ccnnected to mongoDB using mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://divz_here:divz_here@networqed-5dikb.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongoDB
+mongoose
+    .connect(db, {useNewUrlParser:true, useUnifiedTopology: true})
+    .then(()=>console.log('mongodb connected'))
+    .catch(err=>console.log(err))
+
+//server 
 app.get('/', (req,res)=> res.send('Hello World!'));
 
 const port = process.env.PORT || 5000;
